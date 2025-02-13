c45025ca3669dbe4a5cf563c04b05ff430702b02
Pau Vilella <pau.vilella@apple.com>
2025-02-13 18:32:00 +0100

    add 2 more neotree keymaps

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,8 +11,11 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    -- [PVS]: Change the keymap to <leader>ñ because it's easier in ISO ES keyboard layout
-    { '<leader>ñ', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    -- [PVS]: Change the keymap to | as recommended by neo-tree documentation
+    { '|', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    -- [PVS]: Add the following keymaps
+    { '<leader>nb', ':Neotree toggle show buffers right<CR>', desc = 'Neotree show open buffers on the right', silent = true },
+    { '<leader>ng', ':Neotree float git_status<CR>', desc = 'Neotree show git status', silent = true },
   },
   opts = {
     filesystem = {
@@ -20,8 +23,8 @@ return {
         -- [PVS]: Add autoexpand to match filename length
         auto_expand_width = true,
         mappings = {
-          -- [PVS]: Change keymap to <leader>ñ.
-          ['<leader>ñ'] = 'close_window',
+          -- [PVS]: Change the keymap to | as recommended by neo-tree documentation
+          ['|'] = 'close_window',
         },
       },
     },
